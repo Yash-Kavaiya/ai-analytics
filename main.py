e3023c3ce0943e6910b9b9816cf3282cd943777a
Yash Kavaiya <103239923+Yash-Kavaiya@users.noreply.github.com>
2023-11-02 16:35:41 +0000

    Phase-1 Complted

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
-import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
+from pandasai import PandasAI
 from pandasai import SmartDataframe
+from pandasai.llm import GooglePalm
 
-# Create a Streamlit app with a file uploader and a text input box for user queries.
-def main():
-    st.title("CSV Data Analysis")
-    uploaded_file = st.file_uploader("Choose a CSV file")
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        df = SmartDataframe(df)
+llm = GooglePalm(api_key="AIzaSyAnw_3MPaMeoR6zK3Om9ObtaTXLTfuZ86k")
+import matplotlib.pyplot as plt
 
-        # Define a function to process user queries and return the appropriate response.
-        def process_query(query):
-            if "summary" in query.lower():
-                return df.describe().to_html()
-            elif "plot" in query.lower():
-                return "Please enter the column names to plot (comma-separated)"
-            else:
-                return "Sorry, I don't understand that query."
+import seaborn as sns
+from PIL import Image
+st.set_page_config(
+    page_title="PandasAI Insight App",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+styles = """
+<style>
+img {
+    max-width: 50%;
+}
+.sidebar .sidebar-content {
+    background-color: #f5f5f5;
+}
+</style>
+"""
 
-        # Define a function to plot the data based on user input.
-        def plot_data(cols):
-            if len(cols) == 2:
-                plt.plot(df[cols[0]], df[cols[1]])
-                st.pyplot()
-            else:
-                st.write("Please enter two column names separated by a comma.")
+st.sidebar.title('PandasAI Insights')    
+st.sidebar.subheader('AI Insights')
+header = st.container()
 
-        # Add the necessary Streamlit components to display the response and the plot.
-        query = st.text_input("Ask a question about the data")
-        df.chat('Which are the countries with GDP greater than 3000000000000?')
-
-if __name__ == "__main__":
-    main()
+with header:
+    st.title("PandasAI Analysis App")
+    st.markdown("Use this Streamlit app to analyze your data in one shot. You can upload your data and ask questions about it. The app will answer your questions and provide you with insights about your data.")
+    st.write("")
+# Define main content
+content = st.container()
+with content:
+    # Load sales dataset
+    sale_file = st.file_uploader('Select Your Local Sales CSV (default provided)')
+    if sale_file is not None:
+        df = pd.read_csv(sale_file, encoding='latin-1')
+        sdf = SmartDataframe(df, config={"llm": llm})
+        st.dataframe(df)
+        query = st.text_input(label='Enter your query')
+        Analyze = st.button(label='Analyze')
+        if Analyze:
+            result = sdf.chat(query)
+            print(result)
+            st.write(result)
+    else:
+        st.warning("Please select a CSV file to continue.")
+        st.stop()
